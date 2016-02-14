5c9175aae52a912eac913e2230f6e8680f4606b0
Fiskell <stephen.fiskell@gmail.com>
2016-02-13 20:02:20 -0800

    Test for deconstructCall function

--- tests/LaravelQueryBuilderTest.php
+++ tests/LaravelQueryBuilderTest.php
@@ -5,6 +5,13 @@ use TestCase;
 
 class LaravelQueryBuilderTest extends TestCase
 {
+    public function test_call_parts() {
+        $query_part = "table('user')";
+        $parts = Converter::deconstructCall($query_part);
+        $this->assertEquals('table', $parts[0]);
+        $this->assertEquals("'user'", $parts[1]);
+    }
+
     public function test_get_all() {
         $builder = "DB::table('user')->get();";
         $converted = Converter::convert($builder);
