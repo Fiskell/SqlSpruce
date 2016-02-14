1501a4993c761f20780b0645a1a4fcfc7fc983e8
Fiskell <stephen.fiskell@gmail.com>
2016-02-13 20:01:56 -0800

    Add deconstructCall method to get pieces of query builder function call

--- app/Converter/PHP/Laravel/QueryBuilder/Converter.php
+++ app/Converter/PHP/Laravel/QueryBuilder/Converter.php
@@ -1,8 +1,30 @@
 <?php namespace App\Converter\PHP\Laravel\QueryBuilder;
 
+use SelvinOrtiz\Utils\Flux\Flux;
+
 class Converter
 {
     public static function convert($builder) {
         return null;
+        $static_parts = explode('::', $builder);
+        $class        = $static_parts[0];
+        $query        = $static_parts[1];
+
+        $query_parts = explode('->', $query);
+        foreach ($query_parts as $query_part) {
+        }
+    }
+
+    public static function deconstructCall($call) {
+        $flux    = Flux::getInstance()
+                       ->startOfLine()
+                       ->word()
+                       ->find('(')
+                       ->anything()
+                       ->then(')')
+                       ->endOfLine();
+        $pattern = $flux->getPattern();
+        preg_match($pattern, $call, $matches);
+        return [$matches[1], $matches[3]];
     }
 }
