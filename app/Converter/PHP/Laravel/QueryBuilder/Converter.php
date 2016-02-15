b3f2194b9bf540cdcdf2fbfc762476da335cd095
Fiskell <stephen.fiskell@gmail.com>
2016-02-14 23:00:30 -0800

    Convert now can convert simple select query

--- app/Converter/PHP/Laravel/QueryBuilder/Converter.php
+++ app/Converter/PHP/Laravel/QueryBuilder/Converter.php
@@ -1,18 +1,33 @@
 <?php namespace App\Converter\PHP\Laravel\QueryBuilder;
 
+use App\Converter\QueryBuilder;
 use SelvinOrtiz\Utils\Flux\Flux;
 
 class Converter
 {
     public static function convert($builder) {
-        return null;
         $static_parts = explode('::', $builder);
         $class        = $static_parts[0];
         $query        = $static_parts[1];
 
         $query_parts = explode('->', $query);
+
+        $query = new QueryBuilder();
         foreach ($query_parts as $query_part) {
+            $call_parts = self::deconstructCall($query_part);
+            print_r($call_parts);
+            switch($call_parts[0]) {
+                case 'table':
+                    $query->table = self::unquote($call_parts[1]);
+            }
         }
+        return $query->getSelectQuery();
+    }
+
+    public static function unquote($string) {
+        $string = trim($string);
+        $string = trim($string, '\'');
+        return trim($string, '"');
     }
 
     public static function deconstructCall($call) {
@@ -26,7 +41,8 @@ class Converter
 
         $pattern = $flux->getPattern();
         preg_match($pattern, $call, $matches);
+        print_r($matches);
 
-        return [$matches[1], $matches[3]];
+        return [array_get($matches, 1), array_get($matches, 3)];
     }
 }
