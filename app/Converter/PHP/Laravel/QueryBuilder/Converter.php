cd619e7f75ebb56ca9ee506e38ffee7f1f33df1f
Fiskell <stephen.fiskell@gmail.com>
2016-02-15 23:27:48 -0800

    Add debugging code to figure out why newlines break everything

--- app/Converter/PHP/Laravel/QueryBuilder/Converter.php
+++ app/Converter/PHP/Laravel/QueryBuilder/Converter.php
@@ -18,9 +18,13 @@ class Converter
         $query        = $static_parts[1];
 
         $query_parts = explode('->', $query);
+        $query_parts = str_replace("\n", "", $query_parts);
+        print_r($query_parts);
 
         $query = new QueryBuilder();
         foreach ($query_parts as $query_part) {
+            echo 'part' . "\n";
+            print_r($query_part);
             $call_parts = self::deconstructCall($query_part);
             print_r($call_parts);
             switch($call_parts[0]) {
@@ -77,9 +81,8 @@ class Converter
                     ->endOfLine();
 
         $pattern = $flux->getPattern();
-        print_r($pattern);
         preg_match($pattern, $call, $matches);
-        print_r($matches);
+        echo "\n" . $call . ' ' . json_encode($matches);
 
         return [array_get($matches, 1), array_get($matches, 3)];
     }
