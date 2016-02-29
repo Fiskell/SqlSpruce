89e171fbcb850b678dc50ac6dd142937efa0fcd9
Fiskell <stephen.fiskell@gmail.com>
2016-02-28 21:26:37 -0800

    Remove customer query builder class

--- app/Converter/PHP/Laravel/QueryBuilder/Converter.php
+++ app/Converter/PHP/Laravel/QueryBuilder/Converter.php
@@ -1,6 +1,7 @@
 <?php namespace App\Converter\PHP\Laravel\QueryBuilder;
 
-use App\Converter\QueryBuilder;
+use DB;
+use Illuminate\Database\Query\Builder;
 use SelvinOrtiz\Utils\Flux\Flux;
 
 class Converter
@@ -21,27 +22,32 @@ class Converter
         $query_parts = explode('->', $query);
         $query_parts = str_replace("\n", "", $query_parts);
 
-        $query = new QueryBuilder();
+        $query = null;
         foreach ($query_parts as $query_part) {
             $call_parts = self::deconstructCall($query_part);
-            switch($call_parts[0]) {
+            $params_raw     = explode(',', array_get($call_parts, 1));
+            $params = [];
+            foreach($params_raw as $param) {
+                $params[] = self::sanitizeParameter($param);
+            }
+            switch ($call_parts[0]) {
                 case 'table':
-                    $query->setTable($call_parts[1]);
+                    $query = DB::table($params[0]);
                     break;
                 case 'select':
-                    $query->setSelect(explode(',', $call_parts[1]));
+                    $query->select($params);
                     break;
                 case 'distinct':
-                    $query->setDistinct(true);
+                    $query->distinct();
                     break;
                 case 'where':
                     $where_parts = explode(',', $call_parts[1]);
                     $where_count = count($where_parts);
 
-                    if($where_count == 2) {
-                        $query->addAndCondition($where_parts[0], '=', $where_parts[1]);
-                    } else if($where_count == 3) {
-                        $query->addAndCondition($where_parts[0], $where_parts[1], $where_parts[2]);
+                    if ($where_count == 2) {
+                        $query->where($params[0], self::sanitizeValue($params[1]));
+                    } else if ($where_count == 3) {
+                        $query->where($params[0], $params[1], self::sanitizeValue($params[2]));
                     } else {
                         throw new \Exception('Invalid where clause');
                     }
@@ -51,40 +57,39 @@ class Converter
                     $where_parts = explode(',', $call_parts[1]);
                     $where_count = count($where_parts);
 
-                    if($where_count == 2) {
-                        $query->addOrCondition($where_parts[0], '=', $where_parts[1]);
-                    } else if($where_count == 3) {
-                        $query->addOrCondition($where_parts[0], $where_parts[1], $where_parts[2]);
+
+                    if ($where_count == 2) {
+                        $query->orWhere($params[0], self::sanitizeValue($params[1]));
+                    } else if ($where_count == 3) {
+                        $query->orWhere($params[0], $params[1], self::sanitizeValue($params[2]));
                     } else {
                         throw new \Exception('Invalid where clause');
                     }
 
                     break;
-                case 'whereBetween':
-                    $where_parts = explode(',', $call_parts[1]);
-                    $where_count = count($where_parts);
-
-                    if($where_count == 2) {
-                        $query->addWhereBetweenCondition($where_parts[0], $where_parts[1]);
-                    }
-
-                    break;
                 case 'value':
-                    $query->setSelect([$call_parts[1]]);
-                    $query->setIsSelect(true);
+                    $query->select($params[0]);
                     break;
                 case 'groupBy':
-                    $query->setGroupBy($call_parts[1]);
+                    $query->groupBy($params[0]);
                     break;
                 case 'get':
-                    $query->setIsSelect(true);
+                    // DO NOTHING
             }
         }
+        return self::getRawQuery($query);
+    }
 
-        if($query->getIsSelect()) {
-            return $query->getSelectQuery();
-        }
-        return "no query";
+    /**
+     * @param Builder $query
+     * @return string
+     */
+    public static function getRawQuery(Builder $query) {
+        $raw_query   = $query->toSql();
+        $bindings    = $query->getBindings();
+        $bound_query = vsprintf(str_replace("?", "%s", $raw_query), $bindings);
+
+        return $bound_query . ";";
     }
 
     /**
@@ -108,5 +113,36 @@ class Converter
         preg_match($pattern, $call, $matches);
 
         return [array_get($matches, 1), array_get($matches, 3)];
+    }
+
+    /**
+     * Remove starting and ending quotes (single and double)
+     *
+     * @param $string
+     * @return string
+     */
+    public static function unquote($string) {
+        $string = trim($string);
+        $string = trim($string, '\'');
+
+        return trim($string, '"');
+    }
+
+    /**
+     * @param $param
+     * @return string
+     */
+    public static function sanitizeParameter($param) {
+        return self::unquote($param);
+    }
+
+    /**
+     * @param $param
+     * @return int|string
+     */
+    public static function sanitizeValue($param) {
+        $param = self::unquote($param);
+        $param = is_numeric($param) ? $param : "\"$param\"";
+        return $param;
     }
 }
