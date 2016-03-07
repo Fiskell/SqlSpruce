cd9d902d14d18ee30178b3e4c376655b7ebfab6d
Stephen Fiskell <stephen.fiskell@gmail.com>
2016-03-06 18:38:14 -0800

    Add test for group by

--- tests/Laravel/FilterTest.php
+++ tests/Laravel/FilterTest.php
@@ -5,11 +5,22 @@ use TestCase;
 
 class FilterTest extends TestCase
 {
-    public function test_where_three_params() {
-        $builder = "DB::table('users')->orderBy('name', 'desc')->get();";
+    public function test_order_by()
+    {
+        $builder   = "DB::table('users')->orderBy('name', 'desc')->get();";
         $converted = Converter::convert($builder);
 
         $query = "select * from `users` order by `name` desc;";
         $this->assertEquals($converted, $query);
     }
+
+    public function test_group_by()
+    {
+        $builder   = "DB::table('users')->groupBy('account_id')->get();";
+        $converted = Converter::convert($builder);
+
+        $query = "select * from `users` group by `account_id`;";
+        $this->assertEquals($converted, $query);
+    }
+
 }
